46f24cf0b460fffc4fd1174edf94d47d082dca43
Kujogit <s4kujo03@students.osao.fi>
2025-02-05 09:08:31 +0200

    Movement/Controller script
    Opettajan mukaan häärättiin player.controllerin koodipuolella

--- Mepe2D/Assets/PlayerMovement.cs
+++ Mepe2D/Assets/PlayerMovement.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Debug.Log("Morjensta pöytään... t. tietokone");
-    }
+    public PlayerController controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.GetAxisRaw("Horizontal"));
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")) 
+        {
+        crouch = false;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
