ee08fdc33466f63c3b51f2ddbb482863e26974c6
Kujogit <s4kujo03@students.osao.fi>
2025-02-12 13:45:52 +0200

    Main menu :)
    Menu ja nappulat työstettyjä

---
+++ Mepe2D/Assets/MainMenu.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework.Internal.Commands;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject panel;
+    public GameObject settingsPanel;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        panel.SetActive(false);
+    }
+
+    public void OpenSettingsPanel()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettingsPanel()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
